c73c30c07ad203daa44399ef3f2e58f20fa688a8
jacobgives <jacobgives01@gmail.com>
2017-11-09 11:51:25 -0600

    commit 1 of timeZone proj

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  time = new Date();
+  lastTimeZoneSelected = null;
+
+  getDateByZone(timezone) {
+    this.time = new Date();
+    if (timezone === 'MST') {
+      this.time.setHours(this.time.getHours() -1);
+    } else if (timezone === 'CST') {
+      this.time.setHours(this.time.getHours());
+    } else if (timezone === 'EST') {
+      this.time.setHours(this.time.getHours() + 1);
+    }
+    else{
+      this.time.setHours(this.time.getHours() -2);
+    }
+    this.lastTimeZoneSelected = timezone;
+  }
+
 }
